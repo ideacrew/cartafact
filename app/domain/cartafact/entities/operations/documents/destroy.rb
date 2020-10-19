f97adfeaa3c9478ccc7b3958e87dd82c05c05b18
nisanth <nisanth.yaganti@gmail.com>
2020-10-19 11:39:35 -0500

    updating destroy operation

--- app/domain/cartafact/entities/operations/documents/destroy.rb
+++ app/domain/cartafact/entities/operations/documents/destroy.rb
@@ -2,8 +2,8 @@ module Cartafact
   module Entities
     module Operations
       module Documents
-        class Update
-          include Dry::Monads[:result]
+        class Destroy
+          include Dry::Monads[:result, :do, :try, :maybe, :list]
 
           def self.call(input)
             self.new.call(input)
@@ -11,21 +11,19 @@ module Cartafact
 
           def call(input)
             id = yield bson_id_from_params(input[:id])
-            document = yield get_document(id.value!)
+            document = yield get_document(id)
             action = yield destroy_document(document.value!)
             Success()
           end
 
           def bson_id_from_params(id_string)
-            Try do
-              Success(BSON::ObjectId.from_string(id_string))
-            end
+            Success(BSON::ObjectId.from_string(id_string))
           end
 
           def get_document(id)
             Try do
               document = ::Document.where(
-                "_id" => bson_id_from_params(id)
+                "_id" => id
               ).first
 
               if document.present?
@@ -38,9 +36,12 @@ module Cartafact
 
           def destroy_document(document)
             Try do
-              document.destroy!
-              Success()
-            end.or(Failure(document.errors.to_h))
+              if document.destroy!
+                Success()
+              else
+                Failure(document.errors.to_h)
+              end
+            end
           end
         end
       end
